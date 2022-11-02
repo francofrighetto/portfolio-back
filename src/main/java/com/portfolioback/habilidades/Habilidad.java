ed8a6b8b40e05a8cdd306d0df42f150fe9214993
francofrighetto <68360116+francofrighetto@users.noreply.github.com>
2022-11-02 00:44:14 -0300

    g y s hab y pry

--- src/main/java/com/portfolioback/habilidades/Habilidad.java
+++ src/main/java/com/portfolioback/habilidades/Habilidad.java
@@ -28,6 +28,46 @@ public class Habilidad {
     
     @Column
     private String imagen;
+
+    public int getId_habilidad() {
+        return id_habilidad;
+    }
+
+    public void setId_habilidad(int id_habilidad) {
+        this.id_habilidad = id_habilidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getPersona() {
+        return persona;
+    }
+
+    public void setPersona(int persona) {
+        this.persona = persona;
+    }
     
     @Column
     private int persona;
--- src/main/java/com/portfolioback/proyecto/Proyecto.java
+++ src/main/java/com/portfolioback/proyecto/Proyecto.java
@@ -26,6 +26,94 @@ public class Proyecto {
     
     @Column
     private String descripcion;
+
+    public int getId_proyecto() {
+        return id_proyecto;
+    }
+
+    public void setId_proyecto(int id_proyecto) {
+        this.id_proyecto = id_proyecto;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTecnologias() {
+        return tecnologias;
+    }
+
+    public void setTecnologias(String tecnologias) {
+        this.tecnologias = tecnologias;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTexto_link() {
+        return texto_link;
+    }
+
+    public void setTexto_link(String texto_link) {
+        this.texto_link = texto_link;
+    }
+
+    public String getLink2() {
+        return link2;
+    }
+
+    public void setLink2(String link2) {
+        this.link2 = link2;
+    }
+
+    public String getTexto_link2() {
+        return texto_link2;
+    }
+
+    public void setTexto_link2(String texto_link2) {
+        this.texto_link2 = texto_link2;
+    }
+
+    public String getSvg() {
+        return svg;
+    }
+
+    public void setSvg(String svg) {
+        this.svg = svg;
+    }
+
+    public int getPersona() {
+        return persona;
+    }
+
+    public void setPersona(int persona) {
+        this.persona = persona;
+    }
     
     @Column
     private String tecnologias;
